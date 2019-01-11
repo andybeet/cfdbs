47009bae1c0d2a70a2071a03a0e31ebe7fa2bcf1
Andrew Beet <andrew.beet>
2019-01-11 13:41:44 -0500

    cfdbs package help

---
+++ R/cfdbs.R
@@ -0,0 +1,16 @@
+#' cfdbs: Extract info from Database
+#'
+#'Tools to aid in extraction of data from the cfdbs database and ADIOS!
+#'
+#'\itemize{
+#'\item Get_ functions to access, species coes, gear codes, landings etc.
+#'\item Bundled data aggregating area codes to regions and gear codes to fleets
+#'\item Secure login to servers
+#'}
+#'
+#'To learn more about using \code{cfdbs}, start with the vignette: \code{browseVignettes(package="cfdbs")} or click the index below
+#'
+#'
+#' @docType package
+#' @name cfdbs
+NULL
