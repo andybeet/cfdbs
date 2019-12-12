41b797710de63f26da2c9b6f57be733531e0047e
andybeet <22455149+andybeet@users.noreply.github.com>
2019-12-12 17:52:26 -0500

    added NEGEAR as an argument to get_landings - can now search by NEGEAR or NEGEAR2

--- R/get_landings.R
+++ R/get_landings.R
@@ -12,6 +12,7 @@
 #'@param years a numeric vector containing the years to search over
 #'@param tonnage a numeric vector of tonnage classes (toncl2) (1 - 8) where 1 = 10-19 tons, 2 = 20-29 tons etc
 #'@param species a numeric list of species (nespp3 or species_itis)
+#'@param gearCode character string. Relates to the codes supplied in \code{gears}. Either "NEGEAR" or "NEGEAR2".
 #'@param species_itis boolean value. indicating if species values are nespp3 (FALSE) or species_itis (TRUE).Default = FALSE
 #'
 #'@return A list is returned:
@@ -44,7 +45,7 @@
 #'@export
 #'
 
-get_landings <- function(channel,area="all",gear="all",year=1994,tonnage="all",species="all",species_itis=FALSE){
+get_landings <- function(channel,area="all",gear="all",year=1994,tonnage="all",species="all",gearCode="NEGEAR2",species_itis=FALSE){
 
   # create an SQL query to extract all relavent data from tables
   # this will require the user to enter statistical area, EPU, fleet characteristics, species etc.
@@ -53,7 +54,13 @@ get_landings <- function(channel,area="all",gear="all",year=1994,tonnage="all",s
   whereVec <- list()
 
   whereVec[[1]] <-  dbutils::createString(itemName="area",area,convertToCharacter=TRUE,numChars=3)
-  whereVec[[2]] <-  dbutils::createString(itemName="negear2",gear,convertToCharacter=TRUE,numChars=2)
+  if (tolower(gearCode) =="negear2") {
+    whereVec[[2]] <-  dbutils::createString(itemName="negear2",gear,convertToCharacter=TRUE,numChars=2)
+  } else if (tolower(gearCode) =="negear") {
+    whereVec[[2]] <-  dbutils::createString(itemName="negear",gear,convertToCharacter=TRUE,numChars=3)
+  } else {
+    stop(message(paste0("Gear codes must match NEGEAR or NEGEAR2 specs")))
+  }
   if (species_itis == FALSE) {
     whereVec[[3]] <-  dbutils::createString(itemName="nespp3",species,convertToCharacter=TRUE,numChars=3)
   } else {
@@ -79,7 +86,7 @@ get_landings <- function(channel,area="all",gear="all",year=1994,tonnage="all",s
   # eventually user will be able to pass these variables
   sqlStatement <- "select year, month, day, negear, negear2, toncl2, nespp3, nespp4, species_itis, market_code, area, ntrips, mesh,df, da , spplndlb,spplivlb, trplndlb,trplivlb,GIS_LAT,GIS_LON  
                     from stockeff.mv_cf_landings"
-
+  
   sqlStatement <- paste(sqlStatement,whereStr)
 
   # call database
